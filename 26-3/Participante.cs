e5c8f6b88ee5c16ee34a901c8724e6701ec4c64a
EkizdeXD <alexisamena4@gmail.com>
2025-03-26 22:45:45 -0300

    El Comienzo De Un Infierno

---
+++ 26-3/Participante.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _26_3
+{
+    public class Participante
+    {
+        public int NroParticipante { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int Tiempo { get; set; }
+        public int Altura { get; set; }
+        public int Categoria { get; set; }
+    }
+}
